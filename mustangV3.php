374941da533565f8e6cf731cf013ec6bd9447564
mramirez81 <marioramirez@lewisu.edu>
2018-06-27 22:15:58 -0500

    Add files.

--- mustangV3.php
+++ mustangV3.php
@@ -1,18 +1,5 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
-    $myfiler = fopen("contacts.json", "r") or die("Unable to open file to read!");
-    $obj = fread($myfiler,filesize("contacts.json"));
-    fclose($myfiler);
-    echo $obj;
-?>
-<?php
-    $contacts = $_POST["contacts"];
-    $myfile = fopen("contacts.json", "w") or die("Unable to open file to write!");
-    fwrite($myfile, $contacts);
-    fclose($myfile);
-    echo "Contacts saved";
-?>
-<?php
+ 
       
     $cityState = array(
         "60101" => "Addison, IL",
